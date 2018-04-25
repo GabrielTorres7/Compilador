c07f59f16c5a4f518494b9eeafc4c9ccd13f419d
Hiagozeroum <hiagozeroum@gmail.com>
2018-04-25 19:37:56 -0300

    Mudanças Analise de expressao

--- Compilador/src/padrao/AnalisaExpressao.java
+++ Compilador/src/padrao/AnalisaExpressao.java
@@ -9,10 +9,13 @@ import java.lang.Math;
  */
 public class AnalisaExpressao {
         
-    private String expressao;
-    private String expressaoFinal;
+    private String expressao= "";
+    private String expressaoFinal = "";
     private ArrayList<String> letras = new ArrayList();
     private ArrayList <String>numeros = new ArrayList();
+    private ArrayList <String>mulop = new ArrayList();
+    private ArrayList <String>relop = new ArrayList();
+    private ArrayList <String>unop = new ArrayList();
 
 
   
@@ -27,132 +30,34 @@ public class AnalisaExpressao {
         this.expressao = expressao;
         this.analisa(expressao);       
     }
-    private int testaSqrt(int i,String exp,String aux){
-        if( aux.equals("s") && !Aplicacao.variaveis.containsKey(aux) ){                 
-            if(expressao.length()>=5){ //Caso seja menor q a palavra sqrt(
-                if( expressao.charAt(i+1)=='q' && expressao.charAt(i+2)=='r' && expressao.charAt(i+3)=='t' && expressao.charAt(i+4)=='(' ){
-                    //Sqrt( correto          
-                    return 1;
-                } else {
-                     //Invalido Exception
-                    System.out.println("Inválida sqrt1");
-                    System.exit(0);
+    private boolean testaMulop(String exp){
+        if(mulop.contains(exp)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+    private void testaParenteses(String exp){
+        int abertos = 0;
+        int fechados = 0;
+        for (int j = 0; j <exp.length(); j++) {
+            if(String.valueOf(exp.charAt(j)).equals("(")){
+                abertos++;
+                if(String.valueOf(exp.charAt(j+1)).equals(")"))
+                    throw new RuntimeException("Expressao invalida! (parenteses)");
+            }
+            if(String.valueOf(exp.charAt(j)).equals(")")){
+                if(abertos>fechados)
+                    fechados++;
+                else{
+                    throw new RuntimeException("Expressao invalida! (parenteses)");
                 }
-            }else {
-                 //Invalido Exception
-                    System.out.println("Inválida sqrt2");
-                    System.exit(0);
             }
         }
-        if( aux.equals("q") && !Aplicacao.variaveis.containsKey(aux) ){                 
-            if(expressao.length()>=4){ //Caso seja menor q a palavra sqrt(
-                if( expressao.charAt(i+1)=='r' && expressao.charAt(i+2)=='t' && expressao.charAt(i+3)=='(' ){
-                    //Sqrt( correto
-                    return 1;
-                } else {
-                     //Invalido Exception
-                    System.out.println("Inválida sqrt1");
-                    System.exit(0);
-                }
-            }else {
-                 //Invalido Exception
-                    System.out.println("Inválida sqrt2");
-                    System.exit(0);
-            }
-        }
-        if( aux.equals("r") && !Aplicacao.variaveis.containsKey(aux) ){                 
-            if(expressao.length()>=3){ //Caso seja menor q a palavra sqrt(
-                if( expressao.charAt(i+1)=='t' && expressao.charAt(i+2)=='(' ){
-                    //Sqrt( correto
-                    return 1;
-                } else {
-                     //Invalido Exception
-                    System.out.println("Inválida sqrt1");
-                    System.exit(0);
-                }
-            }else {
-                 //Invalido Exception
-                    System.out.println("Inválida sqrt2");
-                    System.exit(0);
-            }
-        }
-        if( aux.equals("t") && !Aplicacao.variaveis.containsKey(aux) ){                 
-            if(expressao.length()>=2){ //Caso seja menor q a palavra sqrt(
-                if(  expressao.charAt(i+1)=='(' ){
-                    //Sqrt( correto
-                    return 1;
-                } else {
-                     //Invalido Exception
-                    System.out.println("Inválida sqrt1");
-                    System.exit(0);
-                }
-            }else {
-                 //Invalido Exception
-                    System.out.println("Inválida sqrt2");
-                    System.exit(0);
-            }
-        }
+        if(abertos!=fechados)
+            throw new RuntimeException("Expressao invalida! (parenteses)");
         
-        //Nao e sqrt
-        return 0;
-    }
-    private int testaNot(int i,String exp,String aux){
-        if( aux.equals("n") && !Aplicacao.variaveis.containsKey(aux) ){
-            if(expressao.length()>=4){ // Caso seja menor q a palavra not(
-                if( expressao.charAt(i+1)=='o' && expressao.charAt(i+2)=='t' && expressao.charAt(i+3)=='(' ){
-                //Not correto
-               return 1;
-                } else {
-                     //Invalido Exception
-                    System.out.println("Inválido not1");
-                    System.exit(0);
-                }
-            }else {
-               //Invalido Exception
-               System.out.println("Inválido not2");
-               System.exit(0);
-            }    
-        }
-    // Nao é not
-    return 0;
-    }
-    private int testaMod(int i,String exp,String aux){
-        if( aux.equals("m") && !Aplicacao.variaveis.containsKey(aux) ){
-            if(expressao.length()>=4){ // Caso seja menor q a palavra mod(
-                if( expressao.charAt(i+1)=='o' && expressao.charAt(i+2)=='d' && expressao.charAt(i+3)=='(' ){
-                    return 1;
-                } else {
-                     //Invalido Exception
-                    System.out.println("Inválido mod1");
-                    System.exit(0);
-                }
-            } else{
-                   //Invalido Exception
-                   System.out.println("Inválido mod2");
-                   System.exit(0);
-            }
-        }         
-    //Nao e mod    
-    return 0;
-    }
-    private int testaOr(int i,String exp,String aux){
-       if( aux.equals("o") && !Aplicacao.variaveis.containsKey(aux) ){
-            if(expressao.length()>=2){ // Caso seja menor q a palavra mod(
-                if( expressao.charAt(i+1)=='r' ){
-                    return 1;
-                } else {
-                     //Invalido Exception
-                    System.out.println("Inválido or1");
-                    System.exit(0);
-                }
-            } else{
-                   //Invalido Exception
-                   System.out.println("Inválido or2");
-                   System.exit(0);
-            }
-        } 
-      //Nao e or
-      return 0;
     }
 
     private boolean testaSeVariavel(String aux){
@@ -169,7 +74,7 @@ public class AnalisaExpressao {
     public Object analisa (String expressao){
         Variavel a  = new Variavel(teste1,"a");
         Variavel b =  new Variavel(teste2,"b");
-        Variavel c =  new Variavel(teste2,"c");
+        Variavel variav =  new Variavel(teste2,"variav");
         
         
         // Preenchendo lista com letras de a-z
@@ -180,60 +85,86 @@ public class AnalisaExpressao {
         // Preenchendo lista com algarismos de 0-9
         numeros.add("0"); numeros.add("1"); numeros.add("2"); numeros.add("3"); numeros.add("4");
         numeros.add("5"); numeros.add("6"); numeros.add("7"); numeros.add("8"); numeros.add("9"); 
+        //Preenchendo Mulop
+        mulop.add("*"); mulop.add("/"); mulop.add("mod"); mulop.add("div"); mulop.add("and");
+        //Preenchendo relop
+        relop.add("=");relop.add("<");relop.add("<=");relop.add(">");relop.add(">=");relop.add("<>");
         
         //TESTE
         System.out.println("Inicial = "+expressao);
+        testaParenteses(expressao);
         
         for(i=0; i<expressao.length(); i++){
-            String aux  = "";
-            aux += expressao.charAt(i); //Aux é uma String de somente um char, que no caso o sendo avaliado nomomento
-            
-            if(i==0) { //Primeiro elemento da exp.
-                if(letras.contains(aux) || numeros.contains(aux) || aux.equals("+") || aux.equals("-") || aux.equals("(") ){
-                    //Se Nao for Sqrt ou not, testa se é variavel
-                  
-                    if(testaSqrt(i, expressao, aux)==0 && testaNot(i, expressao, aux) ==0){
-                       if( testaSeVariavel(aux)){
-                           
-                       }
-                    }
-                }else {
-                    //INVALIDO EXCEPTION
-                    System.out.println("Expressao INVALIDA (1ºelemento)");
-                    System.exit(0);
-                }                                
-            }else // Fim 1º elemento
-            if(letras.contains(aux) || numeros.contains(aux) || aux.equals("+") || aux.equals("-") || aux.equals("*")
-                || aux.equals("/") || aux.equals("=") || aux.equals("<") || aux.equals(">") || aux.equals("(") || aux.equals(")") ){
-                //Se valor anterior foi variavel                
-                if(testaSeVariavel(String.valueOf(expressao.charAt(i-1))) && 
-                   testaSqrt(i, expressao, aux)==0 && testaMod(i, expressao, aux)==0   ) {
-                    
-                    
-                 }
-              
-                if(testaSqrt(i, expressao, aux)==0 && testaNot(i, expressao, aux) ==0){
-                    if(testaSeVariavel(aux)){
-                    }
+            String aux = String.valueOf(expressao.charAt(i)); //Aux é uma String de somente um char, que no caso o sendo avaliado nomomento
+            //Testa se é variavel
+            if(letras.contains(aux)){
+                int contAux = i;
+                String palavraAux = "";
+                String caractere = palavraAux;
+                do{
+                    caractere = String.valueOf(expressao.charAt(contAux));
+                    palavraAux+=caractere;
+                    contAux++;
+                  //  System.out.println("cont="+contAux+"  carac="+caractere);
+                    //Enquanto n for "(" ou ")", n for numero, n for + ou - , n for mulop ou relop, e n for maior
+                } while (!caractere.equals("(")&&!numeros.contains(caractere)&&!caractere.equals("+")&&!caractere.equals("-")&&!mulop.contains(caractere)
+                 && !relop.contains(caractere) && !caractere.equals(")") && contAux<expressao.length());
+                System.out.println("teste="+palavraAux.charAt(2));
+                System.out.println("palvra="+palavraAux);
+                
+                i=contAux;
+                if(Aplicacao.variaveis.containsKey(palavraAux)){
+                
                 }
-                    //mod
-                              
-            }else{
-                //Exception
-                System.out.println("Expressao invalida!! ");
-                System.exit(0);
             }
+         //   System.out.println("assdasdadsasda");;
+            if( i==0 ) { //Primeiro elemento da exp.  
+                 int contAux = i;
+                 String palavraAux = "";
+                 String caractere = palavraAux;
+                 do {                          
+                    if(caractere.equals(","))
+                        throw new RuntimeException("Expressao invalida!");
+                    palavraAux+=String.valueOf(expressao.charAt(contAux));
+                    contAux++;
+                    caractere = String.valueOf(expressao.charAt(contAux));                       
+                 } while(!caractere.equals("(")&&!numeros.contains(caractere)&&!caractere.equals("+")&&!caractere.equals("-")&&!mulop.contains(caractere)
+                         && !relop.contains(caractere));
+                 if(mulop.contains(palavraAux))
+                    throw new RuntimeException("Expressao invalida!");
+                   i=contAux;
+                  
+            }
+            if(expressao.charAt(i-1)=='(' || expressao.charAt(i-1)==')' ){
+                 int contAux = i;
+                 String palavraAux = "";
+                 String caractere = palavraAux;
+                 do {                          
+                   if(caractere.equals(","))
+                        throw new RuntimeException("Expressao invalida!");
+                    palavraAux+=String.valueOf(expressao.charAt(contAux));
+                    contAux++;
+                    caractere = String.valueOf(expressao.charAt(contAux)); 
+                 
+                 } while(!caractere.equals("(")&&!numeros.contains(caractere)&&!caractere.equals("+")&&!caractere.equals("-")&&!mulop.contains(caractere)
+                         && !relop.contains(caractere) && !caractere.equals(")"));
+                 if(mulop.contains(palavraAux))
+                    throw new RuntimeException("Expressao invalida!");
+                 i=contAux;
+            }
+            
+                
+                
+                
+                    
+            
     }//for
         //TESTE expfinal
         System.out.println("final = "+expressao);
         return null;
     }    
+
+
 }
 
-
-/* Impedimentos Atuais 
--N sei como fazer logica dos parenteses () 
--Quando se tem exemplo: sqrt , 0. MAS DA ERRO NO T
-    
-
-*/
+   
