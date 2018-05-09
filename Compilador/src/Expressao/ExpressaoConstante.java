f82012751ce932ec7d668bd26d410136b5b28144
Arthur Codama <arthur_codama@hotmail.com>
2018-05-09 19:26:39 -0300

    Criando a classe ExpressaoConstante

---
+++ Compilador/src/Expressao/ExpressaoConstante.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Expressao;
+
+/**
+ *
+ * @author Arthur
+ */
+public class ExpressaoConstante extends Expressao{
+    
+    public ExpressaoConstante(ResolveExpressao resolve, String expressao) {
+        super(null, expressao);
+    }
+    
+    @Override
+    public Object getResultado(){
+        return super.getExpressao();
+    }
+}
